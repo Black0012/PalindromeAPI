6adebedfea6614c6b6d2cd6e0140e6c995a29b97
Black0012 <49108423+Black0012@users.noreply.github.com>
2024-07-24 15:35:20 +0200

    Add files via upload

---
+++ palindrome/Controllers/PalindromeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace palindrome.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalindromeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return BadRequest("Input string cannot be null or empty.");
+            }
+
+            var reversedString = ReverseString(input);
+            var isPalindrome = IsPalindrome(input);
+
+            var result = new
+            {
+                ReversedString = reversedString,
+                IsPalindrome = isPalindrome
+            };
+
+            // Return the result as JSON
+            return new JsonResult(result);
+        }
+
+        private string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private bool IsPalindrome(string input)
+        {
+            var reversedString = ReverseString(input);
+            return string.Equals(input, reversedString, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
